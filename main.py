e20fb509c8d191ca20fb4348ad110fc483254fe9
kirill <kirisa49@gmail.com>
2026-06-08 04:09:08 +0700

    updated main

--- main.py
+++ main.py
@@ -2,13 +2,36 @@ import asyncio
 from app.utils.recorder_service import RecorderService
 from app.transcription.whisper import transcribe_whisper_cpp
 
+from app.utils.yougile.service import YougileClient
+from app.utils.yougile.executor import YouGileExecutor
+from app.llm.service import LLMService
+
+from config import settings
+
 async def main(url):
     service = RecorderService()
     file = await service.record(url)
 
     print("DONE:", file)
-    print(transcribe_whisper_cpp(audio_path=file))
+    text = transcribe_whisper_cpp(audio_path=file)
+    print(text)
 
+    yc = YougileClient(token=settings.YOUGILE_TOKEN)
+    llm = LLMService(model="qwen2.5:3b")
+    executor = YouGileExecutor(client=yc)
+
+    result = await llm.chat(messages=[
+        {
+            "role": "user",
+            "content": text
+        }
+    ])
+
+    parsed = result.get("parsed")
+    exec_result = await executor.execute(parsed)
+
+    print("\n===== EXECUTOR RESULT =====\n")
+    print(exec_result)
 
 if __name__ == "__main__":
     import sys
