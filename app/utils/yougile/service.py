cc830038d86ac8557a311f96255a77f4755570f6
kirill <kirisa49@gmail.com>
2026-06-08 02:57:28 +0700

    added yougile base api

---
+++ app/utils/yougile/service.py
@@ -0,0 +1,45 @@
+import httpx
+
+class YougileClient:
+    def __init__(self, token: str):
+        self.base_url = "https://ru.yougile.com/api-v2"
+        self.token = token
+
+    async def _request(self, method: str, path: str, json=None):
+        async with httpx.AsyncClient() as client:
+            resp = await client.request(
+                method,
+                self.base_url + path,
+                headers={
+                    "Authorization": f"Bearer {self.token}",
+                    "Content-Type": "application/json"
+                },
+                json=json
+            )
+
+            resp.raise_for_status()
+            return resp.json()
+        
+    async def create_task(self, title: str, column_id: str, deadline: int, assigned=None):
+        return await self._request(
+            "POST",
+            "/tasks",
+            json={
+                "title": title,
+                "columnId": column_id,
+                "assigned": assigned or [],
+                "deadline": {
+                    "deadline": deadline
+                }
+            }
+        )
+    
+    async def set_task_complete(self, task_id: str):
+        return await self._request(
+            "PUT",
+            f"/tasks/{task_id}",
+            json={
+                "completed": True
+            }
+        )
+    
